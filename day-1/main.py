1a5c03f2547ea29d502459d22d2377e6ccd104e0
Monty Hollings <hollings@pm.me>
2021-12-04 22:57:37 +0000

    initial commit

---
+++ day-1/main.py
@@ -0,0 +1,16 @@
+inputFile = open('input.txt', 'r')
+
+x = 0
+y = 0
+z = 0
+for line in inputFile.read().splitlines():
+	a, b = line.split()
+    if a[0] == 'f':
+        x += int(b)
+        y += aim * int(b)
+    elif a[0] == 'u':
+        aim -= int(b)
+    elif a[0] == 'd':
+        aim += int(b)
+print(x * y)
+inputfile.close()
